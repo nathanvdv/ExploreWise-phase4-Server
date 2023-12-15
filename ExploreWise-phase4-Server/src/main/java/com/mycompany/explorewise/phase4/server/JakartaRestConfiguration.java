0b589db0d87f876f3a72a52a95d63eda373ccc1f
nathanvdv <nathan.vandeven00@gmail.com>
2023-12-15 10:49:45 +0100

    Initialization of the project

---
+++ ExploreWise-phase4-Server/src/main/java/com/mycompany/explorewise/phase4/server/JakartaRestConfiguration.java
@@ -0,0 +1,13 @@
+package com.mycompany.explorewise.phase4.server;
+
+import jakarta.ws.rs.ApplicationPath;
+import jakarta.ws.rs.core.Application;
+
+/**
+ * Configures Jakarta RESTful Web Services for the application.
+ * @author Juneau
+ */
+@ApplicationPath("resources")
+public class JakartaRestConfiguration extends Application {
+    
+}
